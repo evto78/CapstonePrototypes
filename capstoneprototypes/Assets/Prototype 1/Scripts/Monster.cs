7dd88c735901271afb9c5dda75463bf48f64dc03
evto78 <150751428+evto78@users.noreply.github.com>
2026-09-10 16:04:52 -0400

    little work on proto2

--- capstoneprototypes/Assets/Prototype 1/Scripts/Monster.cs
+++ capstoneprototypes/Assets/Prototype 1/Scripts/Monster.cs
@@ -5,6 +5,8 @@ using UnityEngine.UI;
 
 public class Monster : MonoBehaviour
 {
+    public SpriteRenderer mySprite;
+    public List<Sprite> sprites;
     public Timeline timeline;
     public PlayerManager player;
     public Image attackCircle;
@@ -15,12 +17,22 @@ public class Monster : MonoBehaviour
     private void Start()
     {
         timeline.AddMarkers(atkSpd);
+        mySprite.sprite = sprites[0];
     }
     void Update()
     {
         if (atkCooldown >= 1)
         {
+            mySprite.sprite = sprites[1];
             Attack();
+        }
+        else if (atkCooldown < 0.1f)
+        {
+            //mySprite.sprite = sprites[1];
+        }
+        else
+        {
+            mySprite.sprite = sprites[0];
         }
         
         attackCircle.fillAmount = atkCooldown;
